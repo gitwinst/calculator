14fd9522b2dd302fc8c8e8d54dcc789d6c06f696
winst <winst.williams@gmail.com>
2023-09-23 17:30:37 -0400

    Fully functioning calculator.

--- calculator-2.js
+++ calculator-2.js
@@ -24,10 +24,9 @@ const resultDisplay = document.getElementById('result');
 /* Calculation Object */
 
 const calc = {
-    num1: '0',
-    num2: '0',
+    num1: null,
+    num2: null,
     op: null,
-    currentNum: 'num1', // unused atm
     continuingCalculation: false
 };
 
@@ -46,26 +45,48 @@ function handleOperand(e) {
 
     const newNum = e.target.textContent;
 
-
     if (calc.op === null && calc.continuingCalculation === true) {
-        calc.num1 = '0' + newNum;
+
+        if (newNum === '.') {
+            calc.num1 = '0' + newNum;
+        } else {
+            calc.num1 = newNum;
+        }
+
         calc.continuingCalculation = false;
         console.log(`Num1: ${Number(calc.num1)}`);
         display.sub = '';
-        display.main = calc.num1.substring(1);
-    } else if (calc.op === null) {
-        if (newNum === '.' && calc.num1.includes('.')) return;
+        display.main = calc.num1;
 
-        calc.num1 += newNum;
+    } else if (calc.op === null) {
+        // checks that num1 is not null, that user just entered '.', and num1 isn't already a float
+        if (calc.num1 && newNum === '.' && calc.num1.includes('.')) return;
+        
+        if (calc.num1 === null && newNum === '.') {
+            calc.num1 = '0' + newNum;
+        } else if (calc.num1 === null) {
+            calc.num1 = newNum;
+        } else {
+            calc.num1 += newNum;
+        }
+
         console.log(`Num1: ${calc.num1}`);
         display.sub = '';
-        display.main = calc.num1.substring(1);
-    } else {
-        if (newNum === '.' && calc.num2.includes('.')) return;
+        display.main = calc.num1;
 
-        calc.num2 += newNum;
+    } else {
+        if (calc.num2 && newNum === '.' && calc.num2.includes('.')) return;
+
+        if (calc.num2 === null && newNum === '.') {
+            calc.num2 = '0' + newNum;
+        } else if (calc.num2 === null) {
+            calc.num2 = newNum;
+        } else{
+            calc.num2 += newNum;
+        }
+
         console.log(`Num2: ${Number(calc.num2)}`);
-        display.main = calc.num2.substring(1);
+        display.main = calc.num2;
     }
 
     updateDisplay();
@@ -73,6 +94,13 @@ function handleOperand(e) {
 
 function handleOperator(e) {
     if (e.target.id === 'operators') return; // ignores clicks on container div
+
+    if (calc.num1 === null) { // if operator is set before num1, assume num1 is 0
+        calc.num1 = '0';
+    }
+    if (calc.num2 !== null) { // if num2 is set and then operator is set again, clear num2
+        calc.num2 = null;
+    }
 
     calc.op = operations.find( operation => operation.name === e.target.id );
 
@@ -100,8 +128,8 @@ function calculate() {
 }
 
 function readyToCalculate() {
-    if (calc.op === null) return false;
-    if (calc.num2.length < 2) return false;
+    if (calc.op === null) return false; // checks that operation is set (and by extension operand1, which must be set before the operation)
+    if (calc.num2 === null) return false; // checks that operand 2 is set
     
     console.log('ready to calculate...');
     return true;
@@ -122,15 +150,15 @@ function divideByZero() {
 }
 
 function resetCalculation() {
-    calc.num1 = '0';
-    calc.num2 = '0';
+    calc.num1 = null;
+    calc.num2 = null;
     calc.op = null;
     result = null;
 }
 
 function prepareForNextCalculation() {
     calc.num1 = String(result);
-    calc.num2 = '0';
+    calc.num2 = null;
     calc.op = null;
     calc.continuingCalculation = true;
     result = null;
@@ -144,7 +172,27 @@ function allClear() {
 }
 
 function clearEntry() {
+    if (calc.num2) {
+        calc.num2 = calc.num2.slice(0, calc.num2.length-1);
+        if (calc.num2.length === 0) {
+            calc.num2 = null;
+        }
+        display.main = calc.num2;
 
+    } else if (calc.op) {
+        calc.op = null;
+        display.sub = '';
+        display.main = calc.num1;
+
+    } else if (calc.num1) {
+        calc.num1 = calc.num1.slice(0, calc.num1.length-1);
+        if (calc.num1.length === 0) {
+            calc.num1 = null;
+        }
+        display.main = calc.num1;
+    }
+
+    updateDisplay();
 }
 
 /* Data Output */
